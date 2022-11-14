adf5e5cb850c7fae05bdb2bc2419106599568aca
dasubermanmind <ero33@drexel.edu>
2022-11-14 16:33:42 -0500

    Added the dreaded css. NGL Looks pretty good

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    # making the default route be pointing to the blogs
+    path('', include('blog.urls', namespace='blog'))
 ]
