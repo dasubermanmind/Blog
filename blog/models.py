f6ba0dee1454598dbc5320e31aeabae42df8e73b
dasubermanmind <ero33@drexel.edu>
2022-11-14 15:11:25 -0500

    Added the ability to comment on a blog post and added all html files associated with it

--- blog/models.py
+++ blog/models.py
@@ -11,12 +11,9 @@ class PublishManager(models.Manager):
 
 
 class Post(models.Model):
-    
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
-    
-    
     
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
@@ -44,4 +41,27 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    
+    
+class Comment(models.Model):
+    # define the relationship to the post data model
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+        
+    def __str__(self) -> str:
+        return f'Comment by {self.name} on {self.post}'
+    
     
